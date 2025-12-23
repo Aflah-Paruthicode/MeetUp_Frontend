f3c2d1785102498e6267f1b5ca22881687dc5c53
Muhammed Aflah TK <aflutech2@gmail.com>
2025-12-23 16:51:27 +0530

    fixing corrupted config

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Navbar from "./Navbar";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <h1>Hey bruh</h1>
-    </div>
+    <BrowserRouter basename="/">
+      <Routes >
+        <Route path="/login" element={<div>Login page</div>} />
+        <Route path="/home" element={<div>Login page</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
