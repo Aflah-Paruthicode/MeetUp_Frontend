52b2edeb2f110282ebf56105ebe0bd43e8f3fd8f
Muhammed Aflah TK <aflutech2@gmail.com>
2026-01-02 14:16:28 +0530

    Connected to the backend,

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-  <legend className="fieldset-legend">Login</legend>
+  const [email, setEmail] = useState("aflu@gmail.com");
+  const [password, setPassword] = useState("Aflu@123");
 
-  <label className="label">Email</label>
-  <input type="email" className="input" placeholder="Email" />
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:8080/login", { email, password }, { withCredentials: true });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  <label className="label">Password</label>
-  <input type="password" className="input" placeholder="Password" />
+  return ( 
+    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 mx-auto my-auto">
+      <legend className="fieldset-legend">Login</legend>
 
-  <button className="btn btn-neutral mt-4">Login</button>
-</fieldset>
-  )
-}
+      <label className="label">Email</label>
+      <input type="email" className="input" name="email" onChange={(e) => setEmail(e.target.value)} value={email} placeholder="Email" />
 
-export default Login
+      <label className="label">Password</label>
+      <input type="password" className="input" name="password" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="Password" />
+
+      <button className="btn btn-neutral mt-4" onClick={handleLogin}>
+        Login
+      </button>
+    </fieldset>
+  );
+};
+
+export default Login;
