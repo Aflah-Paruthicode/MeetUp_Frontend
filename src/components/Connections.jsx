e8e786130faaba66bd3e865dfd7349124ea2385f
Aflah-Paruthicode <amaflu4424@gmail.com>
2026-04-13 22:34:23 +0530

    connection slice created, fixed connections not showing error, fetching connections working

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,19 +1,44 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { baseUrl } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addConnections } from '../utils/connectionSlice'
 
-const Connections = () => { 
+const Connections = () => {
 
-    const fetchConnections = async () => {
-        const res = await axios.get(baseUrl + '/user/connections',{ withCredentials: true});
-        console.log(res);
+  const connections = useSelector(store => store.connections)
+  const dispatch = useDispatch();
+
+  const fetchConnections = async () => {
+    try {
+      const res = await axios.get(baseUrl + '/user/connections', { withCredentials: true });
+      dispatch(addConnections(res?.data?.data));
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    useEffect(() => {
-        fetchConnections();
-    }, []);
+  useEffect(() => {
+    fetchConnections();
+  }, []);
+
+  if (!connections) return;
+  if (connections.length == 0) return <h1>No connections found</h1>
+
   return (
-    <div>Connections</div>
+    <div>
+
+      <h1 className='text-5xl'>Connections</h1>
+
+      <div className='text-white'>
+        {connections.map((connection) => (
+          <div key={connection._id}>
+            <img className='w-72 h-screen' src={connection.photoUrl} alt="" />
+            <p>{connection.firstName + " " + connection.lastName}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
