c51d909e3bf571496b7dfd40542b34543a7c55aa
Aflah-Paruthicode <amaflu4424@gmail.com>
2026-04-19 18:47:07 +0530

    authentication redirect bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import "./App.css";
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import appStore from "./utils/appStore";
 import Feed from "./components/Feed";
 import Connections from "./components/Connections";
 import Requests from "./components/Requests";
 import SignUp from "./components/SignUp";
 
-function App() {
+function App() { 
+
+  const userData = useSelector((store) => store.user);
   return (
     <>
       <Provider store={appStore}>
@@ -18,8 +20,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Body />}>
               <Route path="/" element={<Feed />} />
-              <Route path="/login" element={<div className="flex justify-center items-center bg-linear-to-br from-gray-200 to-gray-50 min-h-screen"><Login /></div>} />
-              <Route path="signup" element={<div className="flex justify-center items-center bg-linear-to-br from-gray-200 to-gray-50 min-h-screen"><SignUp /></div>} />
+              <Route path="/login" element={ userData ? <Navigate to={'/'} /> : <div className="flex justify-center items-center bg-linear-to-br from-gray-200 to-gray-50 min-h-screen"><Login /></div>} />
+              <Route path="signup" element={ userData ? <Navigate to={'/'} /> : <div className="flex justify-center items-center bg-linear-to-br from-gray-200 to-gray-50 min-h-screen"><SignUp /></div>} />
               <Route path="/connections" element={<Connections />} />
               <Route path="/profile" element={<Profile />} />
               <Route path="/requests" element={<Requests />} />
